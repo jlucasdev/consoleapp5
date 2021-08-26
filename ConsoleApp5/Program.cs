8cb6b49c6c4290f52d21d227424571bb06659635
João Lucas <joaolucaspdsilva1@gmail.com>
2021-08-25 22:52:20 -0300

    6 commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite " + SAIDA_PROGRAMA + " para sair do programa");
+                int valor = int.Parse(Console.ReadLine());
+
+                if (SAIDA_PROGRAMA == valor) ;
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
